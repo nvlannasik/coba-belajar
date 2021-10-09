72be1208a8c76dd1f1a8a0c55271d11460fe2502
nvlannasik <annasikn@yahoo.com>
2021-10-09 16:49:19 +0700

    menambahkan image

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 
 const styles = StyleSheet.create({
   text_big: {
@@ -10,6 +10,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  gambar: {
+    width: 300,
+    height: 200,
+    resizeMode: "contain",
   }
 })
 
@@ -18,6 +23,7 @@ const HelloWorldApp = () => {
     <View style= {styles.container}>
       <Text style= {styles.text_big}>Hello World!</Text>
       <Text style= {styles.text_big}>Hellow React Native</Text>
+      <Image source={require('./images/logo.png')} style={styles.gambar}></Image>
     </View>
   )
 }
