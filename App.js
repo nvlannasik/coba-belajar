52a528422a6c3427386d9b94db06e071ba601612
nvlannasik <annasikn@yahoo.com>
2021-10-09 16:39:24 +0700

    internal style

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  text_big: {
+    fontWeight: 'bold',
+    fontSize: 23,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+})
 
 const HelloWorldApp = () => {
   return(
-    <View style= {{flex: 1, justifyContent: "center", alignItems: "center"}}>
-      <Text style= {{fontWeight:"bold", fontSize: 32}}>Hello World!</Text>
+    <View style= {styles.container}>
+      <Text style= {styles.text_big}>Hello World!</Text>
+      <Text style= {styles.text_big}>Hellow React Native</Text>
     </View>
   )
 }
