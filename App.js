70a8131b9da5c06e0fe7f8b1a2d068126c1d48bb
nvlannasik <annasikn@yahoo.com>
2021-10-09 19:43:23 +0700

    membuat custom component & appstyle eksternal

--- App.js
+++ App.js
@@ -1,29 +1,12 @@
 import React from "react";
 import { Text, View, StyleSheet, Image } from "react-native";
-
-const styles = StyleSheet.create({
-  text_big: {
-    fontWeight: 'bold',
-    fontSize: 23,
-  },
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  gambar: {
-    width: 300,
-    height: 200,
-    resizeMode: "contain",
-  }
-})
+import { AppHeader, AppContent } from "./AppCustomComponents";
 
 const HelloWorldApp = () => {
   return(
-    <View style= {styles.container}>
-      <Text style= {styles.text_big}>Hello World!</Text>
-      <Text style= {styles.text_big}>Hellow React Native</Text>
-      <Image source={require('./images/logo.png')} style={styles.gambar}></Image>
+    <View >
+      <AppHeader/>
+      <AppContent/>
     </View>
   )
 }
