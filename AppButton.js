481d582d61bb51e68f86b1577ab6794694603b91
nvlannasik <annasikn@yahoo.com>
2021-10-26 23:11:50 +0700

    menambahkan appbutton dan edit index

---
+++ AppButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, Image, Button } from 'react-native';
+
+const AppButton = () => {
+    return (
+        <View>
+            <Text>App Button</Text>
+            <Button tittle="click me"/> 
+        </View>
+    )
+}
+export default AppButton;
